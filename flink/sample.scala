14adecb901576fe7f531295a461a86eddd36d6ab
yanlolo <yanlolo2012@gmail.com>
2015-05-05 16:55:51 +0200

    histo

--- flink/sample.scala
+++ flink/sample.scala
@@ -31,10 +31,6 @@ object WordCount {
 
     //val output = input.groupBy(1).aggregate(Aggregations.SUM, 0).and(Aggregations.MIN, 2)
 
-    // val samples = input.map { s => (s._1, s._2, 1) }
-    //   .groupBy(0)
-    //   .reduce { (s1, s2) => (s1._1, s1._2 + s2._2, s1._3 + s2._3) }
-
     val samples = input.map { s => new Sample(s._1, s._2) }
 
     //val test = samples.sortPartition("feature",Order.ASCENDING).setParallelism(1)
@@ -44,19 +40,46 @@ object WordCount {
     val adjacencySamples = samples
       .groupBy("label").reduceGroup(new GroupReduceFunction[Sample, AdjacencySample] {
         override def reduce(values: Iterable[Sample], out: Collector[AdjacencySample]): Unit = {
-          var outputId = 0
+          var outputId = 0.0
           val outputList = values.asScala map { t => outputId = t.label; new Histo(t.feature, 1) }
           out.collect(new AdjacencySample(outputId, outputList))
         }
       })
 
     val sortedSample = numSample.join(adjacencySamples).where(0).equalTo("label") {
-      (num, adjacenct, out: Collector[AdjacencySample]) =>  
+      (num, adjacenct, out: Collector[AdjacencySample]) =>
         val label = adjacenct.label
-        val features = adjacenct.features.toList.sortBy(_.value)       
-        out.collect(new AdjacencySample(label, features))
+        val features = adjacenct.features.toList.sortBy(-_.value) //descending
+
+        var minIndex = 0
+        var minValue = Integer.MAX_VALUE.toDouble
+        for (i <- 0 to features.size - 2) {
+          if (features(i).value - features(i + 1).value < minValue) {
+            minIndex = i
+            minValue = features(i).value - features(i + 1).value
+          }
+        }
+
+        val newfrequent = features(minIndex).frequent + features(minIndex + 1).frequent
+        val newValue = (features(minIndex).value * features(minIndex).frequent + features(minIndex + 1).value * features(minIndex + 1).frequent) / newfrequent 
+        val newFea = features.take(minIndex) ++ List(Histo(newValue, newfrequent)) ++ features.drop(minIndex + 2)
+
+        out.collect(new AdjacencySample(label, newFea))
     }
 
+    //    val histo = numSample.join(adjacencySamples).where(0).equalTo("label") {
+    //      (num, adjacenct, out: Collector[AdjacencySample]) =>
+    //        val label = adjacenct.label
+    //        val features = adjacenct.features.toList.sortBy(-_.value)
+    //        println(" -----------------features----------------" + features(0))
+    //        
+    //                val finalFea = features.iterate(features.length-numBins){
+    //                  currentFea =>
+    //                    val newFea = currentFea
+    //                }
+    //
+    //        out.collect(new AdjacencySample(label, features))
+    //    }
 
     // emit result
     sortedSample.writeAsCsv(outputPath, "\n", "|")
@@ -72,9 +95,9 @@ object WordCount {
   private var inputPath: String = null
   private var outputPath: String = null
 
-  case class Sample(label: Int, feature: Double)
+  case class Sample(label: Double, feature: Double)
   case class Histo(value: Double, frequent: Double)
-  case class AdjacencySample(label: Int, features: scala.collection.Iterable[Histo])
+  case class AdjacencySample(label: Double, features: scala.collection.Iterable[Histo])
 
   private def parseParameters(args: Array[String]): Boolean = {
     println(" start parse")
@@ -89,14 +112,13 @@ object WordCount {
     }
   }
 
-  private def getDataSet(env: ExecutionEnvironment): DataSet[(Int, Double)] = {
+  private def getDataSet(env: ExecutionEnvironment): DataSet[(Double, Double)] = {
     println(" start input")
-    env.readCsvFile[(Int, Double)](inputPath, fieldDelimiter = " ", lineDelimiter = "\n",
+    env.readCsvFile[(Double, Double)](inputPath, fieldDelimiter = " ", lineDelimiter = "\n",
       includedFields = Array(0, 1))
   }
 
 }
 
-//0|List(Histo(23.0,1.0), Histo(19.0,1.0), Histo(10.0,1.0), Histo(16.0,1.0), Histo(36.0,1.0), Histo(2.0,1.0), Histo(9.0,1.0), Histo(32.0,1.0), Histo(30.0,1.0), Histo(45.0,1.0))
-//1|List(Histo(46.0,1.0), Histo(78.0,1.0), Histo(83.0,1.0), Histo(30.0,1.0), Histo(64.0,1.0), Histo(28.0,1.0), Histo(87.0,1.0), Histo(90.0,1.0), Histo(53.0,1.0), Histo(72.0,1.0))
-
+//0.0|List(Histo(45.0,1.0), Histo(36.0,1.0), Histo(32.0,1.0), Histo(30.0,1.0), Histo(23.0,1.0), Histo(19.0,1.0), Histo(16.0,1.0), Histo(9.5,2.0), Histo(2.0,1.0))
+//1.0|List(Histo(90.0,1.0), Histo(87.0,1.0), Histo(83.0,1.0), Histo(78.0,1.0), Histo(72.0,1.0), Histo(64.0,1.0), Histo(53.0,1.0), Histo(46.0,1.0), Histo(29.0,2.0))
