a3055cc0eb1a8b2de2df372dac4f9f673b367399
yanlolo <yanlolo2012@gmail.com>
2015-05-06 18:28:35 +0200

    uniform

--- flink/sample.scala
+++ flink/sample.scala
@@ -9,6 +9,8 @@ import org.apache.flink.api.common.functions.GroupReduceFunction
 import scala.collection.JavaConverters._
 import java.lang.Iterable
 
+import math._
+
 object WordCount {
 
   def main(args: Array[String]) {
@@ -34,6 +36,8 @@ object WordCount {
     val samples = input.map { s => new Sample(s._1, s._2) }
 
     //val test = samples.sortPartition("feature",Order.ASCENDING).setParallelism(1)
+
+    val totalSample = input.map { s => 1 }.reduce(_ + _).map { s => (0.0, s) }
 
     val numSample = input.map { s => (s._1, 1) }.groupBy(0).sum(1) //grouped by label
 
@@ -98,7 +102,7 @@ object WordCount {
 
     val test = numSample.join(updatedSample).where(0).equalTo("label") {
       (num, sample, out: Collector[Double]) =>
-        var features = sample.features.toList.sortBy(_.value) //ascend
+        val features = sample.features.toList.sortBy(_.value) //ascend
         val len = features.length
 
         val b = 15 // parameter
@@ -134,9 +138,49 @@ object WordCount {
         out.collect(s)
     }
 
+    val uniform = mergedSample.join(totalSample).where(0).equalTo(0) {
+      (sample, tt, out: Collector[List[Double]]) =>
+        val features = sample.features.toList.sortBy(_.value) //ascend
+
+        val len = features.length
+        var u = new Array[Double](numSplit - 1)
+
+        for (j <- 1 to numSplit - 1) {
+          var s = j * tt._2 / numSplit.toDouble
+
+          if (s <= features(0).frequent) {
+            u(j - 1) = features(0).value
+          } else {
+
+            var i = 0
+            var sumP = 0.0
+            // Sum Procedure
+            while (sumP < s) {
+              if (i == 0)
+                sumP += features(i).frequent / 2
+              else
+                sumP += features(i).frequent / 2 + features(i - 1).frequent / 2
+              i += 1
+            }
+            i -= 2
+
+            var d = s - (sumP - features(i + 1).frequent / 2 - features(i).frequent / 2)
+            var a = features(i + 1).frequent - features(i).frequent
+            var b = 2 * features(i).frequent
+            var c = -2 * d
+            var z = if (a == 0) -c / b else (-b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a)
+
+            u(j - 1) = features(i).value + (features(i + 1).value - features(i).value) * z
+          }
+        }
+
+        out.collect(u.toList)
+
+    }
+
     // emit result
     //test.writeAsCsv(outputPath, "\n", "|")
-    test.writeAsText(outputPath)
+    uniform.writeAsText(outputPath)
 
     // execute program
     env.execute(" Decision Tree ")
