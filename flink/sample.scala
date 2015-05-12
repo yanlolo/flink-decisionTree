24aa82d0d17ac03afed5cb1a033b4310b9fb3690
yanlolo <yanlolo2012@gmail.com>
2015-05-12 14:28:53 +0200

    find the split

--- flink/sample.scala
+++ flink/sample.scala
@@ -5,7 +5,8 @@ import org.apache.flink.util.Collector
 import org.apache.flink.api.java.aggregation.Aggregations
 import org.apache.flink.api.common.operators.Order
 
-import org.apache.flink.api.common.functions.GroupReduceFunction
+import org.apache.flink.api.common.functions._
+import org.apache.flink.configuration.Configuration
 import scala.collection.JavaConverters._
 import java.lang.Iterable
 
@@ -44,10 +45,6 @@ object WordCount {
     val totalSample = numSample.cross(numSampleByLabel)
     //(20,(0.0,10))
     //(20,(1.0,10))
-
-    val entropy = totalSample.map { s => s._2._2.toDouble / s._1 }
-      .reduce((s1, s2) => (-s1 * log(s1) - s2 * log(s2)))
-    // 0.6931471805599453
 
     val adjacencySamples = samples
       .groupBy("label").reduceGroup(new GroupReduceFunction[Sample, AdjacencySample] {
@@ -111,7 +108,7 @@ object WordCount {
     }
     //AdjacencySample(0.0,List(Histo(84.5,4.0), Histo(68.0,2.0), Histo(48.0,3.0), Histo(26.75,8.0), Histo(7.0,3.0)))
 
-     val tt = numSample.map { s => (0.0, s) }
+    val tt = numSample.map { s => (0.0, s) }
     val uniform = mergedSample.join(tt).where(0).equalTo(0) {
       (sample, tt, out: Collector[List[Double]]) =>
         val features = sample.features.toList.sortBy(_.value) //ascend
@@ -150,51 +147,117 @@ object WordCount {
         out.collect(u.toList)
     }
     //List(25.91607980660953, 53.839743969093604)
-    
-    
-    //    val sum = numSample.join(updatedSample).where(0).equalTo("label") {
-    //      (num, sample, out: Collector[Double]) =>
-    //        val features = sample.features.toList.sortBy(_.value) //ascend
-    //        val len = features.length
-    //
-    //        val b = 15 // parameter
-    //        var i = 0
-    //        var s = 0.0
-    //        var result = 0.0
-    //
-    //        if (len == 0) {
-    //          result = 0.0
-    //        } else if (b >= features(len - 1).value) {
-    //          result = num._2
-    //        } else if (b < features(0).value) {
-    //          result = 0.0
-    //        } else {
-    //          while (b >= features(i).value) {
-    //            i += 1
-    //          }
-    //          i -= 1
-    //
-    //          val mi = features(i).frequent
-    //          val mii = features(i + 1).frequent
-    //          val pi = features(i).value
-    //          val pii = features(i + 1).value
-    //          val mb = mi + (mii - mi) * (b - pi) / (pii - pi)
-    //          s = (mi + mb) * (b - pi) / (2 * (pii - pi))
-    //
-    //          for (j <- 0 to i - 1) {
-    //            s += features(j).frequent
-    //          }
-    //          s += features(i).frequent / 2
-    //
-    //        }
-    //        out.collect(s)
-    //    }
 
-   
+    val preSum = numSampleByLabel.cross(uniform).map { s => new Test(s._1._1, s._1._2, s._2) }
+    //((0.0, 10, List(25.91607980660953, 53.839743969093604))
+    //((1.0, 10, List(25.91607980660953, 53.839743969093604))
+
+    val sum = preSum.join(updatedSample).where("label").equalTo("label") {
+      (num, sample, out: Collector[(Double, List[Double])]) =>
+        val label = sample.label
+        val features = sample.features.toList.sortBy(_.value) //ascend
+        val len = features.length
+        var s = new Array[Double](num.uniform.length)
+
+        var k = 0
+        for (b <- num.uniform) {
+          var i = 0
+
+          if (len == 0) {
+            s(k) = 0.0
+          } else if (b >= features(len - 1).value) {
+            s(k) = num.frequent
+          } else if (b < features(0).value) {
+            s(k) = 0.0
+          } else {
+            while (b >= features(i).value) {
+              i += 1
+            }
+            i -= 1
+
+            val mi = features(i).frequent
+            val mii = features(i + 1).frequent
+            val pi = features(i).value
+            val pii = features(i + 1).value
+            val mb = mi + (mii - mi) * (b - pi) / (pii - pi)
+            s(k) = (mi + mb) * (b - pi) / (2 * (pii - pi))
+
+            for (j <- 0 to i - 1) {
+              s(k) += features(j).frequent
+            }
+            s(k) += features(i).frequent / 2
+
+          }
+          k += 1
+        }
+
+        out.collect((label, s.toList))
+    }
+    //(0.0, List(5.981117956487145, 10.0))
+    //(1.0, List(0.0, 3.553797318644815))
+
+    val entropyLeft0 = sum.map(s => (s._1, s._2(0), s._2(1)))
+    val entropyLeft1 = entropyLeft0.map(s => (s._2, s._3)).reduce((s1, s2) => (s1._1 + s2._1, s1._2 + s2._2))
+    val entropyLeft2 = entropyLeft0.cross(entropyLeft1).map { s => (s._1._1, s._1._2 / s._2._1, s._1._3 / s._2._2) }
+    //(0.0,1.0,0.7378006152005714)
+    //(1.0,0.0,0.26219938479942856)
+
+    val entropyLeft3 = entropyLeft2.map { s =>
+      var x = 0.0
+      var y = 0.0
+      if (s._2 == 0)
+        x = 0
+      else x = -s._2 * log(s._2)
+      if (s._3 == 0)
+        y = 0
+      else y = -s._3 * log(s._3)
+      (s._1, x, y)
+    }
+    //(0.0, -0.0, 0.22435163581096879)
+    //(1.0, 0.0, 0.3509932206095104)
+
+    val gainLeft = entropyLeft3.map(s => (s._2, s._3)).reduce((s1, s2) => (s1._1 + s2._1, s1._2 + s2._2))
+    //(0.0,0.5753448564204792)
+
+    val entropyRight0 = entropyLeft0.join(numSampleByLabel).where(0).equalTo(0)
+      .map { s => (s._1._1, s._2._2 - s._1._2, s._2._2 - s._1._3) }
+    //(0.0, 4.018882043512855, 0.0)
+    //(1.0, 10.0, 6.446202681355185)
+    val entropyRight1 = entropyRight0.map(s => (s._2, s._3)).reduce((s1, s2) => (s1._1 + s2._1, s1._2 + s2._2))
+    val entropyRight2 = entropyRight0.cross(entropyRight1).map { s => (s._1._1, s._1._2 / s._2._1, s._1._3 / s._2._2) }
+    val entropyRight3 = entropyRight2.map { s =>
+      var x = 0.0
+      var y = 0.0
+      if (s._2 == 0)
+        x = 0
+      else x = -s._2 * log(s._2)
+      if (s._3 == 0)
+        y = 0
+      else y = -s._3 * log(s._3)
+      (s._1, x, y)
+    }
+    val gainRight = entropyRight3.map(s => (s._2, s._3)).reduce((s1, s2) => (s1._1 + s2._1, s1._2 + s2._2))
+    //(0.5991488600923703, 0.0)
+
+    val tau = entropyLeft1.cross(numSample).map { s => (s._1._1 / s._2, s._1._2 / s._2) }
+    //(0.29905589782435726, 0.6776898659322408)
+
+    val entropy = totalSample.map { s => s._2._2.toDouble / s._1 }
+      .reduce((s1, s2) => (-s1 * log(s1) - s2 * log(s2)))
+    // 0.6931471805599453
+
+    val gain = entropy.cross(tau).cross(gainLeft).cross(gainRight)
+      //(((0.6931471805599453,(0.29905589782435726,0.6776898659322408)),(0.0,0.5753448564204792)),(0.5991488600923703,0.0))
+      .map { s => (s._1._1._1 - s._1._1._2._1 * s._1._2._1 - s._1._1._2._2 * s._2._1, s._1._1._1 - s._1._1._2._1 * s._1._2._2 - s._1._1._2._2 * s._2._2) }
+    //(0.2871100698904919,0.521086907964493)
+
+    val split = gain.cross(uniform).map { s => List((s._1._1, s._2(0)), (s._1._2, s._2(1))) }
+      .map { s => s.sortBy(-_._1) }.map(s => s(0)._2)
+    //53.839743969093604
 
     // emit result
     //test.writeAsCsv(outputPath, "\n", "|")
-    uniform.writeAsText(outputPath)
+    split.writeAsText(outputPath)
 
     // execute program
     env.execute(" Decision Tree ")
@@ -210,6 +273,7 @@ object WordCount {
   case class Sample(label: Double, feature: Double)
   case class Histo(value: Double, frequent: Double)
   case class AdjacencySample(label: Double, features: scala.collection.Iterable[Histo])
+  case class Test(label: Double, frequent: Double, uniform: List[Double])
 
   private def parseParameters(args: Array[String]): Boolean = {
     println(" start parse")
@@ -232,15 +296,3 @@ object WordCount {
 
 }
 
-//0.0|List(Histo(45.0,1.0), Histo(32.666666666666664,3.0), Histo(19.333333333333332,3.0), Histo(9.5,2.0), Histo(2.0,1.0))//
-//1.0|List(Histo(84.5,4.0), Histo(72.0,1.0), Histo(64.0,1.0), Histo(49.5,2.0), Histo(29.0,2.0))
-
-//2|List(Histo(45.0,1.0), Histo(32.666666666666664,3.0), Histo(19.333333333333332,3.0), Histo(9.5,2.0), Histo(2.0,1.0), Histo(84.5,4.0), Histo(72.0,1.0), Histo(64.0,1.0), Histo(49.5,2.0), Histo(29.0,2.0))
-//0.0|List(Histo(84.5,4.0), Histo(68.0,2.0), Histo(48.0,3.0), Histo(26.75,8.0), Histo(7.0,3.0))
-
-//sum
-//3.2750646365986786
-//0.0
-
-//uniform
-//List(25.91607980660953, 53.839743969093604)
