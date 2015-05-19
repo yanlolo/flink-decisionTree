e6e6793e8c41a17c86f5a916ee04fd14ece31be8
yanlolo <yanlolo2012@gmail.com>
2015-05-19 11:22:27 +0200

    input

--- flink/testing.scala
+++ flink/testing.scala
@@ -27,12 +27,83 @@ object WordCount {
     // get data set
     val input = getDataSet(env)
     val sample: DataSet[String] = input.flatMap { _.split("\n") } filter { _.nonEmpty }
-    val nonEmptysample: DataSet[List[String]] = sample.map { s => s.split("\t").toList }.filter { !_.contains("") }
-    
+    val nonEmptysample1: DataSet[List[String]] = sample.map { s => s.split("\t").toList }.filter { !_.contains("") }
+
+    def toDouble1(c: Char): Double = {
+      var re = 0.0
+      if (c == '0')
+        re = 0
+      if (c == '1')
+        re = 1
+      if (c == '2')
+        re = 2
+      if (c == '3')
+        re = 3
+      if (c == '4')
+        re = 4
+      if (c == '5')
+        re = 5
+      if (c == '6')
+        re = 6
+      if (c == '7')
+        re = 7
+      if (c == '8')
+        re = 8
+      if (c == '9')
+        re = 9
+      if (c == 'a')
+        re = 10
+      if (c == 'b')
+        re = 11
+      if (c == 'c')
+        re = 12
+      if (c == 'd')
+        re = 13
+      if (c == 'e')
+        re = 14
+      if (c == 'f')
+        re = 15
+
+      re
+    }
+    //Hex
+    //    def toDouble2(s: String): Double = { 
+    //      var re = 0.0
+    //      for (ss <- s) {
+    //        re = re * 16 + toDouble1(ss)
+    //      }
+    //      re
+    //    }
+    def toDouble2(s: String): Double = {
+      var re = 0.0
+      for (ss <- s) {
+        re += toDouble1(ss)
+      }
+      re
+    }
+
+    val nonEmptysample: DataSet[List[Double]] = nonEmptysample1.map { s =>
+      var re = scala.collection.mutable.MutableList[Double]()
+      var i = 0
+
+      for (aa <- s) {
+        if (i <= 13) {
+          re += aa.toDouble
+        } else {
+          re += toDouble2(aa)
+        }
+        i += 1
+      }
+      re.toList
+    }
+
+    val labledSample: DataSet[LabeledVector] = nonEmptysample.map { s =>
+      new LabeledVector(s(0), s.drop(1))
+    }
 
     // emit result
     //sample.writeAsCsv(outputPath, "\n", "|")
-    nonEmptysample.writeAsText(outputPath)
+    labledSample.writeAsText(outputPath)
 
     // execute program
     env.execute(" Decision Tree ")
@@ -50,20 +121,19 @@ object WordCount {
   private val numLevel = 3 // how many levels of tree
   private val leastSample = 5 // least number of samples in one node
 
-  case class LabeledVector(label: Double, vector: Vector) extends Serializable {
-
-    override def equals(obj: Any): Boolean = {
-      obj match {
-        case labeledVector: LabeledVector =>
-          vector.equals(labeledVector.vector) && label.equals(labeledVector.label)
-        case _ => false
-      }
-    }
-
-    override def toString: String = {
-      s"LabeledVector($label, $vector)"
-    }
-  }
+  case class LabeledVector(label: Double, feature: List[Double]) extends Vector
+  //
+  //    override def equals(obj: Any): Boolean = {
+  //      obj match {
+  //        case labeledVector: LabeledVector =>
+  //          vector.equals(labeledVector.vector) && label.equals(labeledVector.label)
+  //        case _ => false
+  //      }
+  //    }
+  //
+  //    override def toString: String = {
+  //      s"LabeledVector($label, $vector)"
+  //    }
 
   //
   //  case class Sample(label: Double, feature: Double)
