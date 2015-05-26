a3207b35aa325aebbe8aec48687751d8378d8435
yanlolo <yanlolo2012@gmail.com>
2015-05-26 11:27:01 +0200

    test

--- flink/testing.scala
+++ flink/testing.scala
@@ -107,162 +107,25 @@ object WordCount {
         }
     }
 
-    //    def add(h1: Histogram, h2: Histogram): Histogram = {
-    //      val h = new Histogram(h1.label, h1.featureIndex, h1.histo ++ h2.histo)
-    //      h
-    //    }
-    //
-    //    val test = doneSample.groupBy("label", "featureIndex") reduce {
-    //      (h1, h2) => add(h1, h2)
-    //    }
-
-    def update(h1: Histogram, h2: Histogram): Histogram = {
-      var re = new Histogram(0, 0, null)
-      var h = (h1.histo ++ h2.histo).toList.sortBy(_.featureValue) //accend
-
-      if (h.size <= numBins) {
-        re = new Histogram(h1.label, h1.featureIndex, h)
-      } else {
-        while (h.size > numBins) {
-          var minIndex = 0
-          var minValue = Integer.MAX_VALUE.toDouble
-          for (i <- 0 to h.size - 2) {
-            if (h(i + 1).featureValue - h(i).featureValue < minValue) {
-              minIndex = i
-              minValue = h(i + 1).featureValue - h(i).featureValue
-            }
-          }
-          val newfrequent = h(minIndex).frequency + h(minIndex + 1).frequency
-          val newValue = (h(minIndex).featureValue * h(minIndex).frequency + h(minIndex + 1).featureValue * h(minIndex + 1).frequency) / newfrequent
-          val newFea = h.take(minIndex) ++ List(Histo(newValue, newfrequent)) ++ h.drop(minIndex + 2)
-          h = newFea
-        }
-        re = new Histogram(h1.label, h1.featureIndex, h)
-      }
-      re
-    }
-
     val updatedSample: DataSet[Histogram] = doneSample.groupBy("label", "featureIndex") reduce {
-      (h1, h2) => update(h1, h2)
-    }
-
-    def merge(m1: MergedHisto, m2: MergedHisto): MergedHisto = {
-      var re = new MergedHisto(0, null)
-      var h = (m1.histo ++ m2.histo).toList.sortBy(_.featureValue) //accend
-      if (h.size <= numBins) {
-        re = new MergedHisto(m1.featureIndex, h)
-      } else {
-        while (h.size > numBins) {
-          var minIndex = 0
-          var minValue = Integer.MAX_VALUE.toDouble
-          for (i <- 0 to h.size - 2) {
-            if (h(i + 1).featureValue - h(i).featureValue < minValue) {
-              minIndex = i
-              minValue = h(i + 1).featureValue - h(i).featureValue
-            }
-          }
-          val newfrequent = h(minIndex).frequency + h(minIndex + 1).frequency
-          val newValue = (h(minIndex).featureValue * h(minIndex).frequency + h(minIndex + 1).featureValue * h(minIndex + 1).frequency) / newfrequent
-          val newFea = h.take(minIndex) ++ List(Histo(newValue, newfrequent)) ++ h.drop(minIndex + 2)
-          h = newFea
-        }
-        re = new MergedHisto(m1.featureIndex, h)
-      }
-      re
+      (h1, h2) => updatePro(h1, h2)
     }
 
     val mergedSample: DataSet[MergedHisto] = updatedSample.map { s => new MergedHisto(s.featureIndex, s.histo) }
       .groupBy("featureIndex") reduce {
-        (m1, m2) => merge(m1, m2)
+        (m1, m2) => mergePro(m1, m2)
       }
 
     val numSample: DataSet[Int] = labledSample.map { s => 1 }.reduce(_ + _)
     //1483
 
     val uniform: DataSet[Uniform] = numSample.cross(mergedSample).map {
-      sample =>
-        val histo = sample._2.histo //ascend
-        val len = histo.length
-        var u = new Array[Double](numSplit - 1)
-
-        for (j <- 1 to numSplit - 1) {
-          var s = j * sample._1 / numSplit.toDouble
-
-          if (s <= histo(0).frequency) {
-            u(j - 1) = histo(0).featureValue
-          } else {
-            val totalSum = (0 until len) map { index => histo(index).frequency } reduce { _ + _ }
-            val extendSum = totalSum - histo(len - 1).frequency / 2
-            if (s >= extendSum) {
-              u(j - 1) = histo(len - 1).featureValue
-            } else {
-              var i = 0
-              var sumP = 0.0
-              //sumPro
-              while (sumP < s) {
-                if (i == 0)
-                  sumP += histo(i).frequency / 2
-                else
-                  sumP += histo(i).frequency / 2 + histo(i - 1).frequency / 2
-                i += 1
-              }
-              i -= 2
-
-              var d = s - (sumP - histo(i + 1).frequency / 2 - histo(i).frequency / 2)
-              var a = histo(i + 1).frequency - histo(i).frequency
-              var b = 2 * histo(i).frequency
-              var c = -2 * d
-              var z = if (a == 0) -c / b else (-b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a)
-
-              u(j - 1) = histo(i).featureValue + (histo(i + 1).featureValue - histo(i).featureValue) * z
-            }
-          }
-        }
-        new Uniform(sample._2.featureIndex, u.toList)
+      sample => uniformPro(sample)
     }
 
     val sum: DataSet[Sum] = uniform.join(updatedSample).where("featureIndex").equalTo("featureIndex") {
       (uni, updatedSample, out: Collector[Sum]) =>
-        val label = updatedSample.label
-        val featureIndex = updatedSample.featureIndex
-        val histo = updatedSample.histo
-
-        val len = histo.length
-        var s = new Array[Double](uni.uniform.length)
-
-        var k = 0
-        for (b <- uni.uniform) {
-          var i = 0
-
-          if (len == 0) {
-            s(k) = 0.0
-          } else if (b <= histo(0).featureValue) {
-            s(k) = 0.0
-          } else if (b >= histo(len - 1).featureValue) {
-            s(k) = (0 until len) map { index => histo(index).frequency } reduce { _ + _ }
-          } else {
-            while (b >= histo(i).featureValue) {
-              i += 1
-            }
-            i -= 1
-
-            val mi = histo(i).frequency
-            val mii = histo(i + 1).frequency
-            val pi = histo(i).featureValue
-            val pii = histo(i + 1).featureValue
-            val mb = mi + (mii - mi) * (b - pi) / (pii - pi)
-            s(k) = (mi + mb) * (b - pi) / (2 * (pii - pi))
-
-            for (j <- 0 to i - 1) {
-              s(k) += histo(j).frequency
-            }
-            s(k) += histo(i).frequency / 2
-
-          }
-          k += 1
-        }
-
-        out.collect(new Sum(label, featureIndex, s.toList))
+        out.collect(sumPro(uni, updatedSample))
     }
 
     val numSampleByLabel: DataSet[(Double, Int)] = labledSample.map { s => (s.label, 1) }.groupBy(0).sum(1)
@@ -294,17 +157,17 @@ object WordCount {
       }
       re
     }
-    //
-    //    val entropy3 = entropy2.groupBy("featureIndex") reduce {
-    //      (h1, h2) => new Sum(0, h1.featureIndex, h1.uniform.zipWithIndex.map { case (e, i) => entropy(e) + entropy(h2.uniform(i)) })
-    //    }
+
+    val entropy3 = entropy2.groupBy("featureIndex") reduce {
+      (h1, h2) => new Sum(0, h1.featureIndex, h1.uniform.zipWithIndex.map { case (e, i) => entropy(e) + entropy(h2.uniform(i)) })
+    }
 
     // emit result
     //test.writeAsText("/home/hadoop/Desktop/test/test")
     updatedSample.writeAsText("/home/hadoop/Desktop/test/updatedSample")
     mergedSample.writeAsText("/home/hadoop/Desktop/test/mergedSample")
-    //    uniform.writeAsText("/home/hadoop/Desktop/test/uniform")
-    //    sum.writeAsText("/home/hadoop/Desktop/test/sum")
+    uniform.writeAsText("/home/hadoop/Desktop/test/uniform")
+    sum.writeAsText("/home/hadoop/Desktop/test/sum")
     //    entropy.writeAsText("/home/hadoop/Desktop/test/entropy")
     //    entropy2.writeAsText("/home/hadoop/Desktop/test/entropy2")
     //    entropy3.writeAsText("/home/hadoop/Desktop/test/entropy3")
@@ -350,6 +213,151 @@ object WordCount {
   private def getDataSet(env: ExecutionEnvironment): DataSet[String] = {
     println(" start input")
     env.readTextFile(inputPath)
+  }
+
+  /*
+   * update process
+   */
+  def updatePro(h1: Histogram, h2: Histogram): Histogram = {
+    var re = new Histogram(0, 0, null)
+    var h = (h1.histo ++ h2.histo).toList.sortBy(_.featureValue) //accend
+
+    if (h.size <= numBins) {
+      re = new Histogram(h1.label, h1.featureIndex, h)
+    } else {
+      while (h.size > numBins) {
+        var minIndex = 0
+        var minValue = Integer.MAX_VALUE.toDouble
+        for (i <- 0 to h.size - 2) {
+          if (h(i + 1).featureValue - h(i).featureValue < minValue) {
+            minIndex = i
+            minValue = h(i + 1).featureValue - h(i).featureValue
+          }
+        }
+        val newfrequent = h(minIndex).frequency + h(minIndex + 1).frequency
+        val newValue = (h(minIndex).featureValue * h(minIndex).frequency + h(minIndex + 1).featureValue * h(minIndex + 1).frequency) / newfrequent
+        val newFea = h.take(minIndex) ++ List(Histo(newValue, newfrequent)) ++ h.drop(minIndex + 2)
+        h = newFea
+      }
+      re = new Histogram(h1.label, h1.featureIndex, h)
+    }
+    re
+  }
+
+  /*
+   * merge process
+   */
+  def mergePro(m1: MergedHisto, m2: MergedHisto): MergedHisto = {
+    var re = new MergedHisto(0, null)
+    var h = (m1.histo ++ m2.histo).toList.sortBy(_.featureValue) //accend
+    if (h.size <= numBins) {
+      re = new MergedHisto(m1.featureIndex, h)
+    } else {
+      while (h.size > numBins) {
+        var minIndex = 0
+        var minValue = Integer.MAX_VALUE.toDouble
+        for (i <- 0 to h.size - 2) {
+          if (h(i + 1).featureValue - h(i).featureValue < minValue) {
+            minIndex = i
+            minValue = h(i + 1).featureValue - h(i).featureValue
+          }
+        }
+        val newfrequent = h(minIndex).frequency + h(minIndex + 1).frequency
+        val newValue = (h(minIndex).featureValue * h(minIndex).frequency + h(minIndex + 1).featureValue * h(minIndex + 1).frequency) / newfrequent
+        val newFea = h.take(minIndex) ++ List(Histo(newValue, newfrequent)) ++ h.drop(minIndex + 2)
+        h = newFea
+      }
+      re = new MergedHisto(m1.featureIndex, h)
+    }
+    re
+  }
+
+  /*
+   * uniform process
+   */
+  def uniformPro(sample: (Int, MergedHisto)): Uniform = {
+    val histo = sample._2.histo //ascend
+    val len = histo.length
+    var u = new Array[Double](numSplit - 1)
+
+    for (j <- 1 to numSplit - 1) {
+      var s = j * sample._1 / numSplit.toDouble
+
+      if (s <= histo(0).frequency) {
+        u(j - 1) = histo(0).featureValue
+      } else {
+        val totalSum = (0 until len) map { index => histo(index).frequency } reduce { _ + _ }
+        val extendSum = totalSum - histo(len - 1).frequency / 2
+        if (s >= extendSum) {
+          u(j - 1) = histo(len - 1).featureValue
+        } else {
+          var i = 0
+          var sumP = 0.0
+          //sumPro
+          while (sumP < s) {
+            if (i == 0)
+              sumP += histo(i).frequency / 2
+            else
+              sumP += histo(i).frequency / 2 + histo(i - 1).frequency / 2
+            i += 1
+          }
+          i -= 2
+
+          var d = s - (sumP - histo(i + 1).frequency / 2 - histo(i).frequency / 2)
+          var a = histo(i + 1).frequency - histo(i).frequency
+          var b = 2 * histo(i).frequency
+          var c = -2 * d
+          var z = if (a == 0) -c / b else (-b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a)
+
+          u(j - 1) = histo(i).featureValue + (histo(i + 1).featureValue - histo(i).featureValue) * z
+        }
+      }
+    }
+    new Uniform(sample._2.featureIndex, u.toList)
+  }
+  /*
+   * sum process
+   */
+  def sumPro(uni: Uniform, updatedSample: Histogram): Sum = {
+    val label = updatedSample.label
+    val featureIndex = updatedSample.featureIndex
+    val histo = updatedSample.histo
+
+    val len = histo.length
+    var s = new Array[Double](uni.uniform.length)
+
+    var k = 0
+    for (b <- uni.uniform) {
+      var i = 0
+
+      if (len == 0) {
+        s(k) = 0.0
+      } else if (b <= histo(0).featureValue) {
+        s(k) = 0.0
+      } else if (b >= histo(len - 1).featureValue) {
+        s(k) = (0 until len) map { index => histo(index).frequency } reduce { _ + _ }
+      } else {
+        while (b >= histo(i).featureValue) {
+          i += 1
+        }
+        i -= 1
+
+        val mi = histo(i).frequency
+        val mii = histo(i + 1).frequency
+        val pi = histo(i).featureValue
+        val pii = histo(i + 1).featureValue
+        val mb = mi + (mii - mi) * (b - pi) / (pii - pi)
+        s(k) = (mi + mb) * (b - pi) / (2 * (pii - pi))
+
+        for (j <- 0 to i - 1) {
+          s(k) += histo(j).frequency
+        }
+        s(k) += histo(i).frequency / 2
+
+      }
+      k += 1
+    }
+    new Sum(label, featureIndex, s.toList)
   }
 
 }
