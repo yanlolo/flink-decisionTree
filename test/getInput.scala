cb2271ffa8de8d968dafa9068a21a5c0761cba31
yanlolo <yanlolo2012@gmail.com>
2015-06-17 18:00:41 +0200

    for R test 1-13

---
+++ test/getInput.scala
@@ -0,0 +1,87 @@
+package org.myorg.quickstart
+
+import org.apache.flink.api.scala._
+import org.apache.flink.util.Collector
+import org.apache.flink.api.java.aggregation.Aggregations
+import org.apache.flink.api.common.operators.Order
+import org.apache.flink.api.common.functions._
+import org.apache.flink.configuration.Configuration
+
+import scala.collection.JavaConverters._
+import java.lang.Iterable
+import math._
+import scala.collection.mutable.ArrayBuffer
+
+object WordCount {
+
+  def main(args: Array[String]) {
+
+    if (!parseParameters(args)) {
+      return
+    }
+
+    // get execution environment
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    // get data set
+    val input = getDataSet(env)
+    val datasets: DataSet[String] = input.flatMap { _.split("\n") } filter { _.nonEmpty }
+    val nonEmptyDatasets = datasets.map { s => s.split("\t") }.filter { !_.contains("") }
+      .map { s => (s(0), s(1), s(2), s(3), s(4), s(5), s(6), s(7), s(8), s(9), s(10), s(11), s(12), s(13)) }
+    //nonEmptyDatasets.writeAsText("/home/hadoop/Desktop/test/nonEmptyDatasets")
+    //.map { s => s.toList }
+    nonEmptyDatasets.writeAsCsv("/home/hadoop/Desktop/test/nonEmptyDatasets", "\n", " ")
+
+    // execute program
+    env.execute(" Decision Tree ")
+  }
+
+  // *************************************************************************
+  //  UTIL Variable
+  // *************************************************************************  
+  private var inputPath: String = null
+  private var outputPath: String = null
+  private val numFeature = 2 // number of independent features
+  private val numBins = 5 // B bins for Update procedure
+  private val numSplit = 3 //By default it should be same as numBins
+  private val numLevel = 1 // how many levels of tree
+  private val leastSample = 5 // least number of samples in one node
+
+  case class LabeledVector(position: String, label: Double, feature: Array[Double])
+  case class Histo(featureValue: Double, frequency: Double)
+  case class Histogram(position: String, label: Double, featureIndex: Int, histo: Array[Histo])
+  case class MergedHisto(position: String, featureIndex: Int, histo: Array[Histo])
+  case class NumSample(position: String, number: Int)
+  case class Uniform(position: String, featureIndex: Int, uniform: Array[Double])
+  case class Sum(position: String, label: Double, featureIndex: Int, sum: Array[Double])
+  case class Gain(position: String, featureIndex: Int, gain: Array[Double])
+  case class Frequency(position: String, label: Double, frequency: Double)
+
+  case class LabeledVectorStr(position: String, label: Double, feature: Array[String])
+  case class HistogramStr(position: String, label: Double, featureIndex: Int, featureValue: String, frequency: Double)
+  case class GainStr(position: String, featureIndex: Int, featureValue: String, gain: Double)
+
+  // *************************************************************************
+  //  UTIL METHODS
+  // *************************************************************************
+
+  private def parseParameters(args: Array[String]): Boolean = {
+    println(" start parse")
+    if (args.length == 2) {
+
+      inputPath = args(0)
+      outputPath = args(1)
+      println(" stop parse")
+      true
+    } else {
+      System.err.println("Please set input/output path. \n")
+      false
+    }
+  }
+
+  private def getDataSet(env: ExecutionEnvironment): DataSet[String] = {
+    println(" start input")
+    env.readTextFile(inputPath)
+  }
+
+}
